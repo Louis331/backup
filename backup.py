dd850a3431cac1f2cd49c1c50d967125425e53d9
Louis331 <louiskirkpatrick@googlemail.com>
2021-02-15 08:49:59 +0000

    working version of the script no auto uploading

--- backup.py
+++ backup.py
@@ -2,8 +2,16 @@ import datetime
 import getpass
 import os
 import shutil
+import time
 
 PATH = os.path.join('C:/Users/{}/AppData/LocalLow/IronGate/Valheim/worlds'.format(getpass.getuser()))
+running = True
 
-now = datetime.datetime.now().strftime("%m-%d-%Y %H %M %S")
-shutil.make_archive('tmp/backup ' + now, 'zip', PATH)
+while running:
+    now = datetime.datetime.now().strftime("%m-%d-%Y %H %M %S")
+    backup_path = os.path.join(os.getcwd(), 'tmp', now)
+
+    shutil.make_archive(backup_path, 'zip', PATH)
+    # os.remove(backup_path + '.zip')
+
+    time.sleep(600)
